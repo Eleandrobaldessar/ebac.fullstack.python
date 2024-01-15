8fca2a2692607bd0a39736ee60951acda423ed10
Eleandro <baldessareleandro@gmail.com>
2024-01-15 14:40:30 -0300

    alteração

--- main.js
+++ main.js
@@ -1,21 +1,31 @@
 const form = document.getElementById('form-comparacao');
-const campoA = document.getElementById('campo-a');
-const campoB = document.getElementById('campo-b');
 
-// evento de submissão ao formulário 
-form.addEventListener('submit', function(e) {
+form.addEventListener('submit', function(e){
     e.preventDefault();
 
-    // parseFloat - garantir que os valores sejam interpretados como números de ponto flutuante
-    const valorCampoA = parseFloat(campoA.value);
-    const valorCampoB = parseFloat(campoB.value);
+    const numeroTotalA = Number(document.getElementById('campo-a').value);
+    const numeroTotalB = Number(document.getElementById('campo-b').value);
+    let formEValido = false;
+    const mensagemSucesso = `O valor de B (<b>${numeroTotalB}</b>) é maior que o valor de A (<b>${numeroTotalA}</b>)`;
+    const mensagemError = `O valor de B (<b>${numeroTotalB}</b>) é menor que o valor de A (<b>${numeroTotalA}</b>)`;
 
-    if (valorCampoA < valorCampoB) {
-        // Ação para quando Campo A for menor que Campo B
-        alert("Campo A deve ser maior ou igual ao Campo B. Formulário não enviado.");
-    } else {
-         // Ação para quando Campo A for maior ou igual a Campo B
-        alert("Formulário enviado com sucesso!");
-        form.submit(); // Envie o formulário se a condição for atendida.
+    function validaNumero(numeroTotalA, numeroTotalB){
+        return numeroTotalA < numeroTotalB;
+    }
+
+        const containerMensagemSucesso = document.querySelector('.success-message');
+        const containerMensagemError = document.querySelector('.error-message');
+    formEValido = validaNumero(numeroTotalA, numeroTotalB);
+    if (formEValido) {
+        containerMensagemSucesso.innerHTML = mensagemSucesso;
+        containerMensagemSucesso.style.display = 'block';
+        containerMensagemError.style.display = 'none';
+        
+    }
+    else {
+        containerMensagemError.innerHTML = mensagemError;
+        containerMensagemError.style.display = 'block';
+        containerMensagemSucesso.style.display = 'none';
+
     }
 });
